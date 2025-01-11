05394c5e2417db751a20adaa3f642ba9759eec5d
parsaex7 <parsaexir2005@gmail.com>
2025-01-11 23:31:18 +0330

    debug

--- fullFreq.v
+++ fullFreq.v
@@ -7,7 +7,7 @@ module fullFreq(
 
 parameter clk_freq = 1000;
 parameter targetFreq = 0.5;
-parameter cycle = clkFerq / targetFreq;
+parameter cycle = clk_freq / targetFreq;
 parameter toggle = cycle / 2;
 parameter blink = 3;
 
@@ -15,7 +15,7 @@ reg flag;
 reg [25:0] repeatCnt;
 reg [25:0] cnt;
 
-always @(posedge rst or posedge clk or posedge full_garage) 
+always @(posedge rst or posedge clk)
 begin
     if (rst)
     begin
@@ -23,33 +23,38 @@ begin
         cnt <= 0;
         repeatCnt <= 0;
     end
-    if (full_garage)
-    begin
-        flag <= 1'b1;
-        cnt <= 0;
-    end
-    if (flag && (repeatCnt == blink))
-    begin
-        cnt <= 0;
-        full_signal <= 1'b0;
-        flag <= 0;
-        repeatCnt <= 0;
-    end
-    else if (flag)
-    begin
-        if (cnt == toggle)
-        begin
-            full_signal <= ~full_signal;
-            repeatCnt <= repeatCnt + 1;
-            cnt <= 0;
-        end
-    end
     else 
     begin
-        cnt <= 0;
-        full_signal <= 1'b0;
-        flag <= 0;
-        repeatCnt <= 0;
+        if (full_garage && ~flag)
+        begin
+            flag <= 1'b1;
+            cnt <= 0;
+            full_signal <= 1'b1;
+            repeatCnt <= 0;
+        end
+        if (flag && (repeatCnt == blink))
+        begin
+            cnt <= 0;
+            full_signal <= 1'b0;
+            flag <= 0;
+            repeatCnt <= 0;
+        end
+        else if (flag)
+        begin
+            if (cnt == toggle)
+            begin
+                full_signal <= ~full_signal;
+                repeatCnt <= repeatCnt + 1;
+                cnt <= 0;
+            end
+        end
+        else 
+        begin
+            cnt <= 0;
+            full_signal <= 1'b0;
+            flag <= 0;
+            repeatCnt <= 0;
+        end
     end
 end
 
