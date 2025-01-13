679a704a5045cd77d5423ae69a1b46bc9bcfebd3
parsaex7 <parsaexir2005@gmail.com>
2025-01-13 21:31:16 +0330

    DONE

--- G5_ParsaExirBahramEmami.v
+++ G5_ParsaExirBahramEmami.v
@@ -498,7 +498,7 @@ module debouncer (
             q2 <= q1;
             q1 <= q0;
             q0 <= sig;
-            sig_debounced <= q0 & ~q2;
+            sig_debounced <= q0 && q1 && ~q2;
         end
     end
 endmodule
@@ -509,67 +509,44 @@ endmodule
 module divider (
     input wire clk,
     input wire reset,
-    output reg clk_4hz,
-    output reg clk_500kHz,
-    output reg clk_60Hz,
-    output reg clk_1hz
+    output reg clk_1kHz,
+    output reg clk_250Hz
 );
 
-parameter clkFerq = 40000000; //40MHz
-parameter targetFreq_4hz = 4;  // 4Hz
-parameter cycle_4hz = clkFerq / targetFreq_4hz;
-parameter toggles_4hz = cycle_4hz / 2;
-parameter targetFreq_500kHz = 500000;  // 500kHz
-parameter cycle_500kHz = clkFerq / targetFreq_500kHz;
-parameter toggles_500kHz = cycle_500kHz / 2;
-parameter targetFreq_60Hz = 60;  // 60Hz
-parameter cycle_60Hz = clkFerq / targetFreq;
-parameter toggles_60Hz = cycle_60Hz / 2;
-parameter targetFreq_1hz = 1;
-parameter cycle_1hz = clkFerq / targetFreq_1hz;
-parameter toggle_1hz = cycle_1hz / 2;
+parameter clkFerq = 40000000; // 40 MHz
 
-reg [25:0] cnt_4hz;
-reg [25:0] cnt_500kHz;
-reg [25:0] cnt_60Hz;
-reg [25:0] cnt_1hz;
+parameter targetFreq_1kHz = 1000; // 1 kHz
+parameter cycle_1kHz = clkFerq / targetFreq_1kHz;
+parameter toggles_1kHz = cycle_1kHz / 2;
 
-always @(posedge clk or posedge reset)
-begin
-    if (reset)
-    begin
-        cnt_4hz <= 0;
-        clk_4hz <= 0;
-        cnt_500kHz <= 0;
-        clk_500kHz <= 0;
-        cnt_60Hz <= 0;
-        clk_60Hz <= 0;
-        cnt_1hz <= 0;
-        clk_1hz <= 0;
-    end else
-    begin
-        cnt_4hz <= cnt + 1;
-        cnt_500kHz <= cnt_500kHz + 1;
-        cnt_60Hz <= cnt_60Hz + 1;
-        cnt_1hz <= cnt_1hz + 1;
-        if (cnt_4hz == toggles_4hz)
-        begin
-            cnt_4hz <= 0;
-            clk_4hz <= ~clk_4hz;
+parameter targetFreq_250Hz = 250; // 250 Hz
+parameter cycle_250Hz = clkFerq / targetFreq_250Hz;
+parameter toggles_250Hz = cycle_250Hz / 2;
+
+reg [25:0] cnt_1kHz;
+reg [25:0] cnt_250Hz;
+
+always @(posedge clk or posedge reset) begin
+    if (reset) begin
+        cnt_1kHz <= 0;
+        clk_1kHz <= 0;
+        cnt_250Hz <= 0;
+        clk_250Hz <= 0;
+    end else begin
+        // 1 kHz clock
+        if (cnt_1kHz < cycle_1kHz - 1) begin
+            cnt_1kHz <= cnt_1kHz + 1;
+        end else begin
+            cnt_1kHz <= 0;
+            clk_1kHz <= ~clk_1kHz;
         end
-        if (cnt_500kHz == toggles_500kHz)
-        begin
-            cnt_500kHz <= 0;
-            clk_500kHz <= ~clk_500kHz;
-        end
-        if (cnt_60Hz == toggles_60Hz)
-        begin
-            cnt_60Hz <= 0;
-            clk_60Hz <= ~clk_60Hz;
-        end
-        if (cnt_1hz == toggle_1hz)
-        begin
-            clk_1hz <= ~clk_1hz;
+
+        // 250 Hz clock
+        if (cnt_250Hz < cycle_250Hz - 1) begin
+            cnt_250Hz <= cnt_250Hz + 1;
+        end else begin
+            cnt_250Hz <= 0;
+            clk_250Hz <= ~clk_250Hz;
         end
     end
 end
@@ -580,75 +557,101 @@ endmodule
 
 module doorFreq(
     input clk,
-    input rst,
+    input rst,  
     input car_in,
     input car_out,
     output reg door_open
 );
 
-    reg tmp = 1'b0;
-    reg [6:0] cnt = 7'b0000000;
-    always @(posedge rst or posedge clk or posedge car_in or posedge car_out)
-    begin
-        if (rst) 
-        begin
-            cnt <= 7'b0000000;
+    parameter CLK_FREQ = 1000;
+    parameter TOGGLE_FREQ = 4; 
+    parameter TOGGLE_COUNT = CLK_FREQ / (2 * TOGGLE_FREQ);
+    parameter TOTAL_TOGGLES = 40;
+
+    reg [25:0] cnt;
+    reg [6:0] toggle_counter;
+    reg toggling;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            cnt <= 0;
+            toggle_counter <= 0;
             door_open <= 0;
-            tmp <= 1'b0;
-        end
-        else if (car_in || car_out)
-        begin
-            tmp <= 1;
-        end 
-        if (tmp && cnt < 7'b1010000) // 80 toggle  because the clock frequency is 4Hz
-        begin
-            door_open <= ~door_open;
-            cnt <= cnt + 1;
-        end
-        else if (cnt == 7'b1010000)
-        begin
-            cnt <= 7'b0000000;
-            door_open <= 0;
-            tmp <= 0;
+            toggling <= 0;
+        end else begin
+            if ((car_in || car_out) && !toggling) begin
+                toggling <= 1;
+                cnt <= 0;
+                toggle_counter <= 0;
+                door_open <= 1;
+            end
+
+            if (toggling) begin
+                if (cnt < TOGGLE_COUNT - 1) begin
+                    cnt <= cnt + 1;
+                end else begin
+                    cnt <= 0;
+                    if (toggle_counter < TOTAL_TOGGLES - 1) begin
+                        door_open <= ~door_open;
+                        toggle_counter <= toggle_counter + 1;
+                    end else begin
+                        door_open <= 0;
+                        toggling <= 0;
+								toggle_counter <= 0;
+                    end
+                end
+            end
         end
     end
+
 endmodule
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
 module fullFreq(
-    input clk,
-    input rst,
-    input full_garage,
-    output reg full_signal
-)
+    input clk,   
+    input rst,  
+    input full_signal,
+    output reg out_signal
+);
 
-reg tmp;
-reg cnt[3:0];
+    parameter TOGGLE_COUNT = 250;
+    parameter TOTAL_TOGGLES = 5;
 
-always @(posedge rst or posedge clk or posedge full_garage) 
-begin
-    if (rst)
-    begin
-        tmp <= 1'b0;
-        cnt <= 4'b0000;
+    reg [25:0] cnt;
+    reg [2:0] toggle_counter;
+    reg toggling;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            cnt <= 0;
+            toggle_counter <= 0;
+            out_signal <= 0;
+            toggling <= 0;
+        end else begin
+            if (full_signal && !toggling) begin
+                toggling <= 1;
+                cnt <= 0;
+                toggle_counter <= 0;
+                out_signal <= 1;
+            end
+
+            if (toggling) begin
+                if (cnt < TOGGLE_COUNT - 1) begin
+                    cnt <= cnt + 1;
+                end else begin
+                    cnt <= 0;
+                    if (toggle_counter < TOTAL_TOGGLES - 1) begin
+                        out_signal <= ~out_signal;
+                        toggle_counter <= toggle_counter + 1;
+                    end else begin
+                        out_signal <= 0;
+                        toggling <= 0;
+                    end
+                end
+            end
+        end
     end
-    if (full_garage)
-    begin
-        tmp <= 1'b1;
-    end
-    if (tmp && cnt < 4'b0011)
-    begin
-        cnt <= cnt + 1;
-        full_signal <= ~full;
-    end 
-    else
-    begin
-        cnt <= 0;
-        tmp <= 0;
-        full_signal <= 1'b0;
-    end
-end
 
 endmodule
 
@@ -663,7 +666,7 @@ module sevenSeg(
 always @(data)
 begin
     case(data)
-    3'b000 : seg <= 8'b01111110;
+    3'b000 : seg <= 8'b00111111;
     3'b001 : seg <= 8'b00000110;
     3'b010 : seg <= 8'b01011011;
     3'b011 : seg <= 8'b01001111;
@@ -697,22 +700,25 @@ always @(posedge clk or posedge rst) begin
         seg_select <= 5'b00001;
         seg_data <= 8'b00000000;
     end
-    else if (space_count == 3'b000) begin
-        if (seg_select == 5'b00001) begin
-            seg_select <= 5'b00100;
-            seg_data <= 8'b00000001;
-        end else if (seg_select == 5'b00100) begin
-            seg_select <= 5'b00001;
-            seg_data <= seg1;
+    else
+    begin
+        if (space_count == 0) begin
+            if (seg_select == 5'b00001) begin
+                seg_select <= 5'b00100;
+                seg_data <= seg1;
+            end else if (seg_select == 5'b00100) begin
+                seg_select <= 5'b00001;
+                seg_data <= 8'b01000000;
+            end
         end
-    end
-    else begin
-        if (seg_select == 5'b00001) begin
-            seg_select <= 5'b00100;
-            seg_data <= seg1;
-        end else if (seg_select == 5'b00100) begin
-            seg_select <= 5'b00001;
-            seg_data <= seg2;
+        else begin
+            if (seg_select == 5'b00001) begin
+                seg_select <= 5'b00100;
+                seg_data <= seg1;
+            end else if (seg_select == 5'b00100) begin
+                seg_select <= 5'b00001;
+                seg_data <= seg2;
+            end
         end
     end
 end
@@ -730,7 +736,8 @@ module main(
     output wire full_signal,
     output wire [3:0] state,
     output wire [4:0] seg_select,
-    output wire [7:0] seg_data
+    output wire [7:0] seg_data,
+	 output tmp
 );
 
     wire full_garage;
@@ -738,38 +745,38 @@ module main(
     wire car_out_deb;
     wire door_open_first;
     wire door_open_exit;
-    wire clk_100hz;
-    wire clk_500kHz;
-    wire clk_60hz;
+    wire clk_1kHz;
+    wire clk_250hz;
     wire [2:0] space_count; // 7 seg
     wire [1:0] near_slot; // 7 seg
+	 assign tmp = rst;
 
     //4 Hz clock
     divider f1(
         .clk(clk_in),
         .reset(rst),
-        .clk_4hz(clk_4hz), // for 2hz blinking
-        .clk_500kHz(clk_500kHz),  // for debouncer
-        .clk_60hz(clk_60hz)   // for 7seg
+        .clk_1kHz(clk_1kHz),  // for debouncer and fsm
+        .clk_250Hz(clk_250hz)   // for 7seg
     );
 
     debouncer f2(
-        .clk(clk_500kHz),
+        .clk(clk_1kHz),
         .reset(rst),
-        .sig(car_in),
+        .sig(~car_in),
         .sig_debounced(car_in_deb)
     );
 
     debouncer f3(
-        .clk(clk_500kHz),
+        .clk(clk_1kHz),
         .reset(rst),
-        .sig(car_out[2]),
+        .sig(~car_out[2]),
         .sig_debounced(car_out_deb)
     );
 
     // change car_in LED to BLINKING mode
+    // TODO: i should change it frequency because some issue
     doorFreq f4(
-        .clk(clk_4hz),
+        .clk(clk_1kHz),
         .rst(rst),
         .car_in(door_open_first),
         .car_out(door_open_exit),
@@ -778,7 +785,7 @@ module main(
 
     // segmentDivider module to generate the 7-segment display data
     segmentDivider f5(
-        .clk(clk_60Hz),
+        .clk(clk_250hz),
         .rst(rst),
         .space_count(space_count),
         .near_slot(near_slot),
@@ -787,21 +794,21 @@ module main(
     );
 
     fullFreq f6(
-        .clk(clk_1hz),
+        .clk(clk_1kHz),
         .rst(rst),
-        .full_garage(full_garage),
-        .full_signal(full_signal)
+        .full_signal(full_garage),
+        .out_signal(full_signal)
     );
 
     FSM f7(
-        .car_in(~car_in_deb),
-        .car_out(~car_out_deb),
-        .car_out_bits(car_out[1:0]),
-        .clk(clk_100hz),
+        .car_in(car_in_deb),
+        .car_out_deb(car_out_deb),
+        .car_out(car_out[1:0]),
+        .clk(clk_1kHz),
         .rst(rst),
         .space_count(space_count),
         .near_slot(near_slot),
-        .door_open_first(door_open_first),
+        .door_open(door_open_first),
         .door_open_exit(door_open_exit),
         .full_garage(full_garage),
         .state(state)
